7c5b28686bcb64a1526bae0aaceb5b1c196f7b2e
Saqib Ansari <saqib.ns111@gmail.com>
2023-10-12 19:53:16 +0500

    image upload changes

--- middleware/uploadImage.js
+++ middleware/uploadImage.js
@@ -1,10 +1,10 @@
 import multer from "multer";
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Define the destination folder
+    cb(null, "public/uploads"); // Define the destination folder
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname); // Use the original file name
+    cb(null, "image-" + Date.now() + "." + file.mimetype.split("/")[1]);
   },
 });
 
